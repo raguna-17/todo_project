91f1fee582259a93cd89fecd2823af20252d8abe
らぐな <pasupoto17@gmail.com>
2025-10-23 22:48:54 +0900

    初回コミット: ToDoアプリ追加

---
+++ tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("toggle/<int:task_id>/", views.toggle_task, name="toggle_task"),
+]
